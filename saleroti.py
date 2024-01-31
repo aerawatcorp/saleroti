e623e998adf639b53b3099b8d1249b2b2309083d
acpmasquerade <dhrubastergmail.com>
2024-01-31 21:57:33 +0545

    cdn sample files

--- saleroti.py
+++ saleroti.py
@@ -1,2 +1,8 @@
 import requests
 
+cdn_meta = "https://cdn.saleroti.com/v1/master.json" # This path should have the lowest TTL  - 300 seconds
+
+meta = requests.get(cdn_meta)
+
+print(meta.json())
+
